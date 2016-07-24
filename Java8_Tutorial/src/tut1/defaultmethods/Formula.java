41ea9b33115c1c62003099e5f05919a529c4edc1
raviasthana <ravi.asthana@gmail.com>
2016-07-24 19:05:39 +0100

    added comments

--- Java8_Tutorial/src/tut1/defaultmethods/Formula.java
+++ Java8_Tutorial/src/tut1/defaultmethods/Formula.java
@@ -3,7 +3,13 @@ package tut1.defaultmethods;
 public interface Formula {
 
 	double calculate(int a);
+	/**
+	 * With Java8 we can add non-abstract method implementation 
+	 * to interface. This feature is also known as "Extension methods"
+	 *
+	 */
 	default double sqrt(int a){
 		return Math.sqrt(a);
 	}
+	
 }
